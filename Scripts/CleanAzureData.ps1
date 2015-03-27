eb5b7c865bac8da1b03b7c43ee28bd917e1c1c4d
unknown <stdimitr@STDIMITR.telerik.com>
2015-03-27 23:30:40 +0200

    Import Modules powershell script

--- Scripts/CleanAzureData.ps1
+++ Scripts/CleanAzureData.ps1
@@ -3,10 +3,7 @@
 	   [Parameter(Mandatory=$True)]$storageAccountName
 )
 
-. "$PSScriptRoot\Common.ps1"
-. "$PSScriptRoot\DatabaseAzure.ps1"
-. "$PSScriptRoot\ManageAzureServices.ps1"
-. "$PSScriptRoot\ManageAzureStorage.ps1"
+. "$PSScriptRoot\Modules.ps1"
 
 DeleteAzureDatabase $config.azure.serverName $azureDatabaseName $config.azure.user $config.azure.password
 
